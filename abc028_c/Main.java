1a24359c23eea662e4d8f7c65ad93e758d6fd4c4
YujiSoftware <yuji.software+github@gmail.com>
2015-12-25 01:07:01 +0900

    Added ABC028_C

---
+++ abc028_c/Main.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] N = new int[] {
+			sc.nextInt(),
+			sc.nextInt(),
+			sc.nextInt(),
+			sc.nextInt(),
+			sc.nextInt()
+		};
+
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				for (int k = 0; k < 5; k++) {
+					if (i != j && j != k && i != k) {
+						set.add(N[i] + N[j] + N[k]);
+					}
+				}
+			}
+		}
+
+		int[] array =
+			set.stream()
+				.sorted((o1, o2) -> o2 - o1)
+				.mapToInt(i -> i)
+				.toArray();
+
+		System.out.println(array[2]);
+	}
+}
