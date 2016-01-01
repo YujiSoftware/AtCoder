d583eb7256b6bfe450625c9b5df35bb47bcfa34d
YujiSoftware <yuji.software+github@gmail.com>
2016-01-02 01:30:13 +0900

    Added ABC019_A

---
+++ abc019_a/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int[] num = new int[3];
+		for(int i = 0; i < num.length; i++){
+			num[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(num);
+		
+		System.out.println(num[1]);
+	}
+}
