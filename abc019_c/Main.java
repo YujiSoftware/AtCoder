a4c93817dbcec2310d8595c623f47f26df6fad61
YujiSoftware <yuji.software+github@gmail.com>
2016-01-05 02:10:46 +0900

    Updated ABC019_C (WA)

--- abc019_c/Main.java
+++ abc019_c/Main.java
@@ -1,14 +1,14 @@
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 
-		Set<Integer> A = new LinkedHashSet<>(N);
+		Set<Integer> A = new TreeSet<>();
 		Set<Integer> used = new HashSet<>(N);
 		for (int i = 0; i < N; i++) {
 			A.add(sc.nextInt());
