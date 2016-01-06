402b468a4c79feb497410536a8baf84cfcd9ab81
YujiSoftware <yuji.software+github@gmail.com>
2016-01-07 00:59:18 +0900

    Added ABC019_C (C#, AC)

---
+++ abc019_c/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int[] A = 
+            Console.ReadLine().Split(' ')
+                .Select(s => int.Parse(s))
+                .OrderBy(i => i)
+                .ToArray();
+
+        HashSet<int> used = new HashSet<int>();
+        int count = 0;
+        foreach(int val in A){
+            if(!used.Contains(val)){
+                count++;
+
+                int temp = val * 2;
+                while(temp <= 1000000000){
+                    used.Add(temp);
+                    temp *= 2;
+                }
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
---
+++ abc019_c/ProgramTest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace AtCoder
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void 入力例1()
+        {
+            string input =
+@"3
+1 2 3";
+            string output =
+@"2";
+
+            AssertIO(input, output);
+        }
+
+        [TestMethod]
+        public void 入力例2()
+        {
+            string input =
+@"4
+2 4 8 16";
+            string output =
+@"1";
+
+            AssertIO(input, output);
+        }
+
+        [TestMethod]
+        public void 入力例3()
+        {
+            string input =
+@"4
+2 3 5 7";
+            string output =
+@"4";
+
+            AssertIO(input, output);
+        }
+
+        private void AssertIO(string input, string output)
+        {
+            StringReader reader = new StringReader(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Program.Main(new string[0]);
+
+            Assert.AreEqual(output + Environment.NewLine, writer.ToString());
+        }
+    }
+}
