dc0875af1d7bb0ab4e4d76f9be3e373cf8e43dce
YujiSoftware <yuji.software+github@gmail.com>
2016-01-10 00:08:56 +0900

    Added ABC033_C (TLE)

---
+++ abc033_c/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int[] s = new int[N];
+		for (int i = 0; i < s.length; i++) {
+			s[i] = sc.nextInt();
+		}
+
+		int length = 0;
+		if (IntStream.of(s).anyMatch(i -> i == 0)) {
+			length = N;
+		} else {
+			boolean[] skip = new boolean[N];
+			for (int i = 0; i < N; i++) {
+				if (s[i] > K) {
+					skip[i] = true;
+				}
+			}
+
+			OuterLoop: for (int i = 1; i <= N; i++) {
+				InnerLoop: for (int j = 0; j <= N - i; j++) {
+					for (int k = i - 1; k >= 0; k--) {
+						if (skip[j + k]) {
+							j += k;
+							continue InnerLoop;
+						}
+					}
+
+					long sum = s[j];
+					for (int k = 1; k < i; k++) {
+						sum *= s[j + k];
+					}
+
+					if (sum <= K) {
+						length = i;
+						continue OuterLoop;
+					}
+				}
+				break;
+			}
+		}
+
+		System.out.println(length);
+	}
+}
---
+++ abc033_c/MainTest.java
@@ -0,0 +1,86 @@
+import static org.hamcrest.CoreMatchers.is;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void 入力例1() throws Exception {
+		String input =
+			"7 6" + System.lineSeparator() +
+				"4" + System.lineSeparator() +
+				"3" + System.lineSeparator() +
+				"1" + System.lineSeparator() +
+				"1" + System.lineSeparator() +
+				"2" + System.lineSeparator() +
+				"10" + System.lineSeparator() +
+				"2";
+		String output =
+			"4";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例2() throws Exception {
+		String input =
+			"6 10" + System.lineSeparator() +
+				"10" + System.lineSeparator() +
+				"10" + System.lineSeparator() +
+				"10" + System.lineSeparator() +
+				"10" + System.lineSeparator() +
+				"0" + System.lineSeparator() +
+				"10";
+		String output =
+			"6";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例3() throws Exception {
+		String input =
+			"6 9" + System.lineSeparator() +
+				"10" + System.lineSeparator() +
+				"10" + System.lineSeparator() +
+				"10" + System.lineSeparator() +
+				"10" + System.lineSeparator() +
+				"10" + System.lineSeparator() +
+				"10";
+		String output =
+			"0";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例4() throws Exception {
+		String input =
+			"4 0" + System.lineSeparator() +
+				"1" + System.lineSeparator() +
+				"2" + System.lineSeparator() +
+				"3" + System.lineSeparator() +
+				"4";
+		String output =
+			"0";
+
+		assertIO(input, output);
+	}
+
+	private void assertIO(String input, String output) throws Exception {
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		Main.main(new String[0]);
+
+		Assert.assertThat(out.toString(), is(output + System.lineSeparator()));
+	}
+}
