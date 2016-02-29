a15373835b784241253a0da367a438e40de64f25
YujiSoftware <yuji.software+github@gmail.com>
2016-03-01 01:44:32 +0900

    Added ABC032_C (WA)

--- abc032_c/Main.java
+++ abc032_c/Main.java
@@ -18,52 +18,29 @@ public class Main {
 		int length = 0;
 		if (IntStream.of(s).anyMatch(i -> i == 0)) {
 			length = N;
-		} else if (K == 0) {
-			length = 0;
 		} else {
-			boolean[] skip = new boolean[N];
-			for (int i = 0; i < N; i++) {
-				if (s[i] > K) {
-					skip[i] = true;
-				}
-			}
+			int left = 0;
 
-			int next = 0;
-			OuterLoop: for (int i = 1; i <= N; i++) {
-				if (s[next] == 1) {
-					length++;
-					next++;
-					continue;
-				}
+			while (true) {
+				int sum = 1;
+				int right;
 
-				InnerLoop: for (int j = 0; j <= N - i; j++) {
-					for (int k = i - 1; k >= 0; k--) {
-						if (skip[j + k]) {
-							j += k;
-							continue InnerLoop;
-						}
+				for (right = left; right < N; right++) {
+					sum *= s[right];
+					if (sum > K) {
+						break;
 					}
 
-					long sum = s[j];
-					for (int k = 1; k < i; k++) {
-						if (s[j + k] != 1) {
-							sum *= s[j + k];
-						}
-						if (sum > K) {
-							break;
-						}
-					}
-
-					if (sum <= K) {
-						length = i;
-						next = j + i;
-						continue OuterLoop;
-					}
+					length = Math.max(right - left + 1, length);
 				}
-				break;
+
+				if (right == N) {
+					break;
+				}
+
+				left++;
 			}
 		}
-
 		System.out.println(length);
 	}
 }
