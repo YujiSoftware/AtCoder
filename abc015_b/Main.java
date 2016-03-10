92e80c2f7660714e0bc873027fb47939cebd6b5b
YujiSoftware <yuji.software+github@gmail.com>
2016-03-11 02:07:59 +0900

    Added ABC015_B (AC)

---
+++ abc015_b/Main.java
@@ -0,0 +1,93 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] A = sc.nextInt(N);
+
+		int sum = 0;
+		int num = 0;
+
+		for (int i = 0; i < N; i++) {
+			if (A[i] > 0) {
+				sum += A[i];
+				num++;
+			}
+		}
+
+		double avg = (double) sum / num;
+
+		System.out.println((int) Math.ceil(avg));
+	}
+
+	public static class Scanner {
+		private BufferedInputStream inputStream;
+
+		public Scanner(InputStream in) {
+			inputStream = new BufferedInputStream(in);
+		}
+
+		public int nextInt() throws IOException {
+			int num = 0;
+
+			int read = skip();
+			do {
+				num = num * 10 + (read - 0x30);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return num;
+		}
+
+		public int[] nextInt(int n) throws IOException {
+			int[] array = new int[n];
+			for (int i = 0; i < n; i++) {
+				array[i] = nextInt();
+			}
+
+			return array;
+		}
+
+		public long nextLong() throws IOException {
+			long num = 0;
+
+			int read = skip();
+			do {
+				num = num * 10 + (read - 0x30);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return num;
+		}
+
+		public long[] nextLong(int n) throws IOException {
+			long[] array = new long[n];
+			for (int i = 0; i < n; i++) {
+				array[i] = nextLong();
+			}
+
+			return array;
+		}
+
+		public String next() throws IOException {
+			StringBuilder builder = new StringBuilder();
+
+			int read = skip();
+			do {
+				builder.append((char) read);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return builder.toString();
+		}
+
+		private int skip() throws IOException {
+			int read;
+			while ((read = inputStream.read()) <= 0x20)
+				;
+
+			return read;
+		}
+	}
+}
---
+++ abc015_b/MainTest.java
@@ -0,0 +1,45 @@
+import static org.hamcrest.CoreMatchers.is;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void 入力例1() throws Exception {
+		String input =
+			"4" + System.lineSeparator() +
+				"0 1 3 8";
+		String output =
+			"4";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例2() throws Exception {
+		String input =
+			"5" + System.lineSeparator() +
+				"1 4 9 10 15";
+		String output =
+			"8";
+
+		assertIO(input, output);
+	}
+
+	private void assertIO(String input, String output) throws Exception {
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		Main.main(new String[0]);
+
+		Assert.assertThat(out.toString(), is(output + System.lineSeparator()));
+	}
+}
