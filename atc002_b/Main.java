a2bf3538bd626e4ff3aa96aab7b533742638f6d1
YujiSoftware <yuji.software+github@gmail.com>
2016-04-12 01:10:06 +0900

    Added ATC002_B (AC)

---
+++ atc002_b/Main.java
@@ -0,0 +1,93 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		long N = sc.nextLong();
+		long M = sc.nextLong();
+		long P = sc.nextLong();
+
+		System.out.println(powMod(N, P, M));
+	}
+
+	private static long powMod(long n, long k, long m) {
+		if (k == 0) {
+			return 1;
+		} else if (k % 2 == 1) {
+			return powMod(n, k - 1, m) * n % m;
+		} else {
+			long t = powMod(n, k / 2, m);
+			return t * t % m;
+		}
+	}
+
+	public static class Scanner {
+		private BufferedInputStream inputStream;
+
+		public Scanner(InputStream in) {
+			inputStream = new BufferedInputStream(in);
+		}
+
+		public int nextInt() throws IOException {
+			int num = 0;
+
+			int read = skip();
+			do {
+				num = num * 10 + (read - 0x30);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return num;
+		}
+
+		public int[] nextInt(int n) throws IOException {
+			int[] array = new int[n];
+			for (int i = 0; i < n; i++) {
+				array[i] = nextInt();
+			}
+
+			return array;
+		}
+
+		public long nextLong() throws IOException {
+			long num = 0;
+
+			int read = skip();
+			do {
+				num = num * 10 + (read - 0x30);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return num;
+		}
+
+		public long[] nextLong(int n) throws IOException {
+			long[] array = new long[n];
+			for (int i = 0; i < n; i++) {
+				array[i] = nextLong();
+			}
+
+			return array;
+		}
+
+		public String next() throws IOException {
+			StringBuilder builder = new StringBuilder();
+
+			int read = skip();
+			do {
+				builder.append((char) read);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return builder.toString();
+		}
+
+		private int skip() throws IOException {
+			int read;
+			while ((read = inputStream.read()) <= 0x20)
+				;
+
+			return read;
+		}
+	}
+}
