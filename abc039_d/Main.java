4c45fce1eb6c0fbbd3e1fce278e3ed96952c94f6
YujiSoftware <yuji.software+github@gmail.com>
2016-06-13 02:23:35 +0900

    Updated ABC039_D (AC)

--- abc039_d/Main.java
+++ abc039_d/Main.java
@@ -1,222 +1,185 @@
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.BitSet;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		int W = sc.nextInt();
-		int H = sc.nextInt();
-		BitSet[] S = new BitSet[H];
-		for (int i = 0; i < H; i++) {
-			S[i] = new BitSet(W);
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int W = sc.nextInt();
+        char[][] S = new char[H][];
+        for (int i = 0; i < H; i++) {
+            S[i] = sc.next().toCharArray();
+        }
 
-			char[] line = sc.next().toCharArray();
-			for (int j = 0; j < W; j++) {
-				if (line[j] == '#') {
-					S[i].set(j);
-				}
-			}
-		}
+        boolean[][] map = new boolean[H][];
+        for (int i = 0; i < H; i++) {
+            map[i] = new boolean[W];
+        }
 
-		// 復元
-		BitSet[] map = new BitSet[H];
-		for (int i = 0; i < H; i++) {
-			map[i] = new BitSet(W);
-			map[i].or(S[i]);
-		}
+        for (int y = 0; y < H; y++) {
+            for (int x = 0; x < W; x++) {
+                if (S[y][x] == '#') {
+                    boolean success = true;
 
-		for (int i = 0; i < H; i++) {
-			if (i - 1 >= 0) {
-				map[i].and(leftShift(S[i - 1], W));
-				map[i].and(S[i - 1]);
-				map[i].and(rightShift(S[i - 1]));
-			}
-			if (i + 1 < H) {
-				map[i].and(leftShift(S[i + 1], W));
-				map[i].and(S[i + 1]);
-				map[i].and(rightShift(S[i + 1]));
-			}
+                    OuterLoop:
+                    for (int yy = Math.max(0, y - 1); yy < Math.min(H, y + 2); yy++) {
+                        for (int xx = Math.max(0, x - 1); xx < Math.min(W, x + 2); xx++) {
+                            if (S[yy][xx] == '.') {
+                                success = false;
+                                break OuterLoop;
+                            }
+                        }
+                    }
 
-			map[i].and(leftShift(S[i], W));
-			map[i].and(rightShift(S[i]));
-		}
+                    if (success) {
+                        map[y][x] = true;
+                    }
+                }
+            }
+        }
 
-		// 復元結果の作成
-		StringBuilder builder = new StringBuilder();
-		for (int i = 0; i < H; i++) {
-			for (int j = 0; j < W; j++) {
-				builder.append(map[i].get(j) ? '#' : '.');
-			}
-			builder.append(System.lineSeparator());
-		}
+        boolean[][] base = new boolean[H][];
+        for (int i = 0; i < H; i++) {
+            base[i] = new boolean[W];
+        }
 
-		// 収縮
-		BitSet[] base = new BitSet[H];
-		for (int i = 0; i < H; i++) {
-			base[i] = new BitSet(W);
-		}
+        for (int y = 0; y < H; y++) {
+            for (int x = 0; x < W; x++) {
+                if (map[y][x]) {
+                    for (int yy = Math.max(0, y - 1); yy < Math.min(H, y + 2); yy++) {
+                        for (int xx = Math.max(0, x - 1); xx < Math.min(W, x + 2); xx++) {
+                            base[yy][xx] = true;
+                        }
+                    }
+                }
+            }
+        }
 
-		for (int i = 0; i < H; i++) {
-			if (i - 1 >= 0) {
-				base[i].or(leftShift(map[i - 1], W));
-				base[i].or(map[i - 1]);
-				base[i].or(rightShift(map[i - 1]));
-			}
-			if (i + 1 < H) {
-				base[i].or(leftShift(map[i + 1], W));
-				base[i].or(map[i + 1]);
-				base[i].or(rightShift(map[i + 1]));
-			}
+        boolean success = true;
+        for (int y = 0; y < H; y++) {
+            for (int x = 0; x < W; x++) {
+                if ((base[y][x] && S[y][x] == '.') || (!base[y][x] && S[y][x] == '#')) {
+                    success = false;
+                    break;
+                }
+            }
+        }
 
-			base[i].or(leftShift(map[i], W));
-			base[i].or(rightShift(map[i]));
-		}
+        if (success) {
+            System.out.println("possible");
 
-		// 答え合わせ
-		boolean success = true;
-		for (int i = 0; i < H; i++) {
-			S[i].andNot(base[i]);
-			if (!S[i].isEmpty()) {
-				success = false;
-				break;
-			}
-		}
+            for (int y = 0; y < H; y++) {
+                for (int x = 0; x < W; x++) {
+                    System.out.print(map[y][x] ? '#' : '.');
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("impossible");
+        }
 
-		if (success) {
-			System.out.println("possible");
-			System.out.print(builder);
-		} else {
-			System.out.println("impossible");
-		}
-	}
+        if(false) {
+            for (int y = 0; y < H; y++) {
+                for (int x = 0; x < W; x++) {
+                    System.err.print(map[y][x] ? '#' : '.');
+                }
+                System.err.println();
+            }
+            System.err.println();
+        }
+    }
 
-	private static BitSet leftShift(BitSet bits, int W) {
-		BitSet newBits = bits.get(1, bits.size() - 1);
-		if (bits.get(W - 1)) {
-			newBits.set(W - 1);
-		}
+    public static class Pair {
+        private int width;
+        private int height;
 
-		return newBits;
-	}
+        public Pair(int width, int height) {
+            super();
+            this.width = width;
+            this.height = height;
+        }
 
-	private static BitSet rightShift(BitSet bits) {
-		BitSet newBits = new BitSet(bits.size());
-		for (int i = bits.nextSetBit(0); i >= 0; i = bits.nextSetBit(i + 1)) {
-			newBits.set(i + 1);
-		}
-		if (bits.get(0)) {
-			newBits.set(0);
-		}
+        public int getWidth() {
+            return width;
+        }
 
-		return newBits;
-	}
+        public int getHeight() {
+            return height;
+        }
 
-	private static void debug(BitSet[] map, int W) {
-		for (int i = 0; i < map.length; i++) {
-			debug(map[i], W);
-		}
-		System.err.println();
-	}
+        @Override
+        public String toString() {
+            return "{" + width + ", " + height + "}";
+        }
+    }
 
-	private static void debug(BitSet map, int W) {
-		for (int j = 0; j < W; j++) {
-			System.err.print(map.get(j) ? '#' : '.');
-		}
-		System.err.println();
-	}
+    public static class Scanner {
+        private BufferedInputStream inputStream;
 
-	public static class Pair {
-		private int width;
-		private int height;
+        public Scanner(InputStream in) {
+            inputStream = new BufferedInputStream(in);
+        }
 
-		public Pair(int width, int height) {
-			super();
-			this.width = width;
-			this.height = height;
-		}
+        public int nextInt() throws IOException {
+            int num = 0;
 
-		public int getWidth() {
-			return width;
-		}
+            int read;
+            while ((read = inputStream.read()) <= 0x20)
+                ;
 
-		public int getHeight() {
-			return height;
-		}
+            do {
+                num = num * 10 + (read - 0x30);
+            } while ((read = inputStream.read()) > 0x20);
 
-		@Override
-		public String toString() {
-			return "{" + width + ", " + height + "}";
-		}
-	}
+            return num;
+        }
 
-	public static class Scanner {
-		private BufferedInputStream inputStream;
+        public int[] nextInt(int n) throws IOException {
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextInt();
+            }
 
-		public Scanner(InputStream in) {
-			inputStream = new BufferedInputStream(in);
-		}
+            return array;
+        }
 
-		public int nextInt() throws IOException {
-			int num = 0;
+        public long nextLong() throws IOException {
+            long num = 0;
 
-			int read;
-			while ((read = inputStream.read()) <= 0x20)
-				;
+            int read;
+            while ((read = inputStream.read()) <= 0x20)
+                ;
 
-			do {
-				num = num * 10 + (read - 0x30);
-			} while ((read = inputStream.read()) > 0x20);
+            do {
+                num = num * 10 + (read - 0x30);
+            } while ((read = inputStream.read()) > 0x20);
 
-			return num;
-		}
+            return num;
+        }
 
-		public int[] nextInt(int n) throws IOException {
-			int[] array = new int[n];
-			for (int i = 0; i < n; i++) {
-				array[i] = nextInt();
-			}
+        public long[] nextLong(int n) throws IOException {
+            long[] array = new long[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextLong();
+            }
 
-			return array;
-		}
+            return array;
+        }
 
-		public long nextLong() throws IOException {
-			long num = 0;
+        public String next() throws IOException {
+            StringBuilder builder = new StringBuilder();
 
-			int read;
-			while ((read = inputStream.read()) <= 0x20)
-				;
+            int read;
+            while ((read = inputStream.read()) <= 0x20)
+                ;
 
-			do {
-				num = num * 10 + (read - 0x30);
-			} while ((read = inputStream.read()) > 0x20);
+            do {
+                builder.append((char) read);
+            } while ((read = inputStream.read()) > 0x20);
 
-			return num;
-		}
-
-		public long[] nextLong(int n) throws IOException {
-			long[] array = new long[n];
-			for (int i = 0; i < n; i++) {
-				array[i] = nextLong();
-			}
-
-			return array;
-		}
-
-		public String next() throws IOException {
-			StringBuilder builder = new StringBuilder();
-
-			int read;
-			while ((read = inputStream.read()) <= 0x20)
-				;
-
-			do {
-				builder.append((char) read);
-			} while ((read = inputStream.read()) > 0x20);
-
-			return builder.toString();
-		}
-	}
+            return builder.toString();
+        }
+    }
 }
