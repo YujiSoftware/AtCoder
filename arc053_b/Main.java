e4b33e9a4a402d4a7cebc92f1eacdf6ba9d52828
YujiSoftware <yuji.software+github@gmail.com>
2016-06-23 00:58:16 +0900

    Updated ARC053_B (AC)

--- arc053_b/Main.java
+++ arc053_b/Main.java
@@ -3,117 +3,102 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		String S = sc.next();
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.next();
 
-		Map<Boolean, List<Long>> collect = S.chars().boxed()
-				.collect(Collectors.groupingBy(i -> i, Collectors.counting()))
-				.entrySet().stream()
-				.map(Map.Entry::getValue)
-				.collect(Collectors.partitioningBy(c -> c % 2 == 1));
+        long[] collect = S.chars().boxed()
+                .collect(Collectors.groupingBy(i -> i, Collectors.counting()))
+                .values().stream()
+                .mapToLong(Long::longValue)
+                .toArray();
 
-		List<Long> oddList = collect.get(true);
-		List<Long> evenList = collect.get(false);
+        long odd = 0;
+        long even = 0;
+        for (long count : collect) {
+            odd += count % 2;
+            even += count / 2;
+        }
 
-		long min = Long.MAX_VALUE;
-		if (oddList.size() == 0) {
-			// 偶数の長さのものをすべて結合したもの
-			// ex: aaaaaabbbbcc -> aaabbccbbaaa
-			min = evenList.stream().mapToLong(Long::longValue).sum();
-		} else {
-			// 偶数文字の方が多ければ、短い偶数文字をまとめて奇数文字と同じ個数する
-			while (oddList.size() < evenList.size()) {
-				Collections.sort(evenList);
-				evenList.add(evenList.remove(0) + evenList.remove(0));
-			}
+        if (odd == 0) {
+            System.out.println(even * 2);
+        } else if (odd > even) {
+            System.out.println(1);
+        } else if (odd == even) {
+            System.out.println(3);
+        } else {
+            System.out.println((even / odd) * 2  + 1);
+        }
+    }
 
-			// 短い奇数文字と長い偶数文字を組み合わせて回分を作る
-			Collections.sort(oddList);
-			Collections.sort(evenList, Comparator.reverseOrder());
-			while (!evenList.isEmpty()) {
-				min = Math.min(min, oddList.remove(0) + evenList.remove(0));
-			}
+    public static class Scanner {
+        private BufferedInputStream inputStream;
 
-			// 奇数が余るようであれば、それは単独の回文として扱う
-			if(!oddList.isEmpty()){
-				min = Math.min(min, oddList.remove(0));
-			}
-		}
+        public Scanner(InputStream in) {
+            inputStream = new BufferedInputStream(in);
+        }
 
-		System.out.println(min);
-	}
+        public int nextInt() throws IOException {
+            int num = 0;
 
-	public static class Scanner {
-		private BufferedInputStream inputStream;
+            int read = skip();
+            do {
+                num = num * 10 + (read - 0x30);
+            } while ((read = inputStream.read()) > 0x20);
 
-		public Scanner(InputStream in) {
-			inputStream = new BufferedInputStream(in);
-		}
+            return num;
+        }
 
-		public int nextInt() throws IOException {
-			int num = 0;
+        public int[] nextInt(int n) throws IOException {
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextInt();
+            }
 
-			int read = skip();
-			do {
-				num = num * 10 + (read - 0x30);
-			} while ((read = inputStream.read()) > 0x20);
+            return array;
+        }
 
-			return num;
-		}
+        public long nextLong() throws IOException {
+            long num = 0;
 
-		public int[] nextInt(int n) throws IOException {
-			int[] array = new int[n];
-			for (int i = 0; i < n; i++) {
-				array[i] = nextInt();
-			}
+            int read = skip();
+            do {
+                num = num * 10 + (read - 0x30);
+            } while ((read = inputStream.read()) > 0x20);
 
-			return array;
-		}
+            return num;
+        }
 
-		public long nextLong() throws IOException {
-			long num = 0;
+        public long[] nextLong(int n) throws IOException {
+            long[] array = new long[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextLong();
+            }
 
-			int read = skip();
-			do {
-				num = num * 10 + (read - 0x30);
-			} while ((read = inputStream.read()) > 0x20);
+            return array;
+        }
 
-			return num;
-		}
+        public String next() throws IOException {
+            StringBuilder builder = new StringBuilder();
 
-		public long[] nextLong(int n) throws IOException {
-			long[] array = new long[n];
-			for (int i = 0; i < n; i++) {
-				array[i] = nextLong();
-			}
+            int read = skip();
+            do {
+                builder.append((char) read);
+            } while ((read = inputStream.read()) > 0x20);
 
-			return array;
-		}
+            return builder.toString();
+        }
 
-		public String next() throws IOException {
-			StringBuilder builder = new StringBuilder();
+        private int skip() throws IOException {
+            int read;
+            while ((read = inputStream.read()) <= 0x20)
+                ;
 
-			int read = skip();
-			do {
-				builder.append((char) read);
-			} while ((read = inputStream.read()) > 0x20);
-
-			return builder.toString();
-		}
-
-		private int skip() throws IOException {
-			int read;
-			while ((read = inputStream.read()) <= 0x20)
-				;
-
-			return read;
-		}
-	}
+            return read;
+        }
+    }
 }
