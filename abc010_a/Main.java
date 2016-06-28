fc78963ace5e5dcfa90744a9090e70beead0036e
YujiSoftware <yuji.software+github@gmail.com>
2016-06-29 02:05:02 +0900

    Added ABC010_A (AC)

---
+++ abc010_a/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		String S = sc.nextLine();
+
+		System.out.println(S + "pp");
+	}
+}
