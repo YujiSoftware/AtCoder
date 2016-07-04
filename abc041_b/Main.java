01b4a3bd0e98c9a9068dcdf55a68b82a46de0692
YujiSoftware <yuji.software+github@gmail.com>
2016-07-05 02:37:46 +0900

    Added ABC041_B (AC)

---
+++ abc041_b/Main.java
@@ -0,0 +1,88 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+
+	private static final int MOD = 1000000007;
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		long A = sc.nextInt();
+		long B = sc.nextInt();
+		long C = sc.nextInt();
+
+		long sum = 0;
+		sum = ((A % MOD) * (B % MOD)) % MOD;
+		sum = (sum * (C % MOD)) % MOD;
+
+		System.out.println(sum);
+	}
+
+	public static class Scanner {
+		private BufferedInputStream inputStream;
+
+		public Scanner(InputStream in) {
+			inputStream = new BufferedInputStream(in);
+		}
+
+		public int nextInt() throws IOException {
+			int num = 0;
+
+			int read = skip();
+			do {
+				num = num * 10 + (read - 0x30);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return num;
+		}
+
+		public int[] nextInt(int n) throws IOException {
+			int[] array = new int[n];
+			for (int i = 0; i < n; i++) {
+				array[i] = nextInt();
+			}
+
+			return array;
+		}
+
+		public long nextLong() throws IOException {
+			long num = 0;
+
+			int read = skip();
+			do {
+				num = num * 10 + (read - 0x30);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return num;
+		}
+
+		public long[] nextLong(int n) throws IOException {
+			long[] array = new long[n];
+			for (int i = 0; i < n; i++) {
+				array[i] = nextLong();
+			}
+
+			return array;
+		}
+
+		public String next() throws IOException {
+			StringBuilder builder = new StringBuilder();
+
+			int read = skip();
+			do {
+				builder.append((char) read);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return builder.toString();
+		}
+
+		private int skip() throws IOException {
+			int read;
+			while ((read = inputStream.read()) <= 0x20)
+				;
+
+			return read;
+		}
+	}
+}
