73879178ae0c3da8fb1b4b0a0b16594594354793
YujiSoftware <yuji.software+github@gmail.com>
2016-07-24 00:51:10 +0900

    Added ABC042_A (AC)

---
+++ abc042_a/MainTest.java
@@ -0,0 +1,38 @@
+import static org.hamcrest.CoreMatchers.is;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+	public void 入力例_1() throws Exception {
+		String input = "5 5 7";
+		String output = "YES";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_2() throws Exception {
+		String input = "7 7 5";
+		String output = "NO";
+
+		assertIO(input, output);
+	}
+
+	private void assertIO(String input, String output) throws Exception {
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		Main.main(new String[0]);
+
+		Assert.assertThat(out.toString(), is(output + System.lineSeparator()));
+	}
+}
