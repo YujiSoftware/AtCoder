8e8d99d8edfc6f01aa36c849d2aa3ea3099cfac4
YujiSoftware <yuji.software+github@gmail.com>
2016-07-30 01:48:42 +0900

    Added ARC028_A (AC)

---
+++ arc028_a/MainTest.java
@@ -0,0 +1,43 @@
+import static org.hamcrest.CoreMatchers.is;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void 入力例1() throws Exception {
+		String input =
+				"5 1 2";
+		String output =
+				"Bug";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例2() throws Exception {
+		String input =
+				"10 3 4";
+		String output =
+				"Ant";
+
+		assertIO(input, output);
+	}
+
+	private void assertIO(String input, String output) throws Exception {
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		Main.main(new String[0]);
+
+		Assert.assertThat(out.toString(), is(output + System.lineSeparator()));
+	}
+}
