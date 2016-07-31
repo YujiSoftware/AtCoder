7d788c63720b1833d63a087b4a74e1e071ab86f7
YujiSoftware <yuji.software+github@gmail.com>
2016-08-01 00:49:05 +0900

    Added AGC002_C (WA)

---
+++ agc002_c/Main.java
@@ -0,0 +1,133 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int L = sc.nextInt();
+		int[] a = new int[N];
+		for (int i = 0; i < N; i++) {
+			a[i] = sc.nextInt();
+		}
+
+		int left = 0;
+		int right = N - 1;
+		long sum = 0;
+		for (int i = 0; i < a.length; i++) {
+			sum += a[i];
+		}
+
+		List<Integer> list = new ArrayList<>();
+		while (left < right) {
+			if (sum < L) {
+				System.out.println("Impossible");
+				return;
+			}
+
+			boolean removeLeft = false;
+			int count = 1;
+			if (a[left] == a[right]) {
+				for (int i = 1; left + i < right - i; i++) {
+					if (a[left + i] < a[right - i]) {
+						removeLeft = true;
+						break;
+					}
+					count++;
+				}
+			} else if (a[left] < a[right]) {
+				removeLeft = true;
+			}
+
+			for (int i = 0; i < count; i++) {
+				if (removeLeft) {
+					list.add(left + 1);
+
+					sum -= a[left];
+					left++;
+				} else {
+					list.add(right);
+
+					sum -= a[right];
+					right--;
+				}
+			}
+		}
+
+		System.out.println("Possible");
+		System.out.println(
+				list.stream().map(i -> i.toString()).collect(Collectors.joining(System.lineSeparator())));
+	}
+
+	public static class Scanner {
+		private BufferedInputStream inputStream;
+
+		public Scanner(InputStream in) {
+			inputStream = new BufferedInputStream(in);
+		}
+
+		public int nextInt() throws IOException {
+			int num = 0;
+
+			int read = skip();
+			do {
+				num = num * 10 + (read - 0x30);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return num;
+		}
+
+		public int[] nextInt(int n) throws IOException {
+			int[] array = new int[n];
+			for (int i = 0; i < n; i++) {
+				array[i] = nextInt();
+			}
+
+			return array;
+		}
+
+		public long nextLong() throws IOException {
+			long num = 0;
+
+			int read = skip();
+			do {
+				num = num * 10 + (read - 0x30);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return num;
+		}
+
+		public long[] nextLong(int n) throws IOException {
+			long[] array = new long[n];
+			for (int i = 0; i < n; i++) {
+				array[i] = nextLong();
+			}
+
+			return array;
+		}
+
+		public String next() throws IOException {
+			StringBuilder builder = new StringBuilder();
+
+			int read = skip();
+			do {
+				builder.append((char) read);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return builder.toString();
+		}
+
+		private int skip() throws IOException {
+			int read;
+			while ((read = inputStream.read()) <= 0x20)
+				;
+
+			return read;
+		}
+	}
+}
---
+++ agc002_c/MainTest.java
@@ -0,0 +1,92 @@
+import static org.hamcrest.CoreMatchers.is;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void 入力例1() throws Exception {
+		String input = "3 50" + System.lineSeparator() +
+				"30 20 10";
+		String output = "Possible" + System.lineSeparator() +
+				"2" + System.lineSeparator() +
+				"1";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例2() throws Exception {
+		String input = "2 21" + System.lineSeparator() +
+				"10 10";
+		String output = "Impossible";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例3() throws Exception {
+		String input = "5 50" + System.lineSeparator() +
+				"10 20 30 40 50";
+		String output = "Possible" + System.lineSeparator() +
+				"1" + System.lineSeparator() +
+				"2" + System.lineSeparator() +
+				"3" + System.lineSeparator() +
+				"4";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例4() throws Exception {
+		String input = "4 20" + System.lineSeparator() +
+				"10 5 10 10";
+		String output = "Possible" + System.lineSeparator() +
+				"1" + System.lineSeparator() +
+				"2" + System.lineSeparator() +
+				"3";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例5() throws Exception {
+		String input = "5 20" + System.lineSeparator() +
+				"10 10 10 10 10";
+		String output = "Possible" + System.lineSeparator() +
+				"4" + System.lineSeparator() +
+				"3" + System.lineSeparator() +
+				"2" + System.lineSeparator() +
+				"1";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例6() throws Exception {
+		String input = "3 20" + System.lineSeparator() +
+				"10 10 10";
+		String output = "Possible" + System.lineSeparator() +
+				"2" + System.lineSeparator() +
+				"1";
+
+		assertIO(input, output);
+	}
+
+	private void assertIO(String input, String output) throws Exception {
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		Main.main(new String[0]);
+
+		Assert.assertThat(out.toString(), is(output + System.lineSeparator()));
+	}
+}
