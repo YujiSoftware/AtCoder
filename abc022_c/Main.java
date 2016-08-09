4255487c6ca5b98babeea07b8f42fb5c90cc53fe
YujiSoftware <yuji.software+github@gmail.com>
2016-08-10 01:45:09 +0900

    Updated ABC022_C (TLE)

--- abc022_c/Main.java
+++ abc022_c/Main.java
@@ -1,6 +1,9 @@
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -10,16 +13,108 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
-        int[] u = new int[M];
-        int[] v = new int[M];
-        int[] l = new int[M];
+        Route[] route = new Route[M * 2];
         for (int i = 0; i < M; i++) {
-            u[i] = sc.nextInt();
-            v[i] = sc.nextInt();
-            l[i] = sc.nextInt();
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int l = sc.nextInt();
+
+            int index = i * 2;
+            route[index] = new Route(i, u, v, l);
+            route[index + 1] = new Route(i, v, u, l);
         }
 
+        Map<Integer, List<Route>> map =
+                Stream.of(route).collect(Collectors.groupingBy(Route::getFrom));
 
+        PriorityQueue<Trace> queue = new PriorityQueue<>();
+        queue.add(new Trace());
+
+        while (!queue.isEmpty()) {
+            Trace trace = queue.poll();
+            if (trace.isGoal()) {
+                System.err.println(trace.routed.toString());
+                System.out.println(trace.length);
+                return;
+            }
+
+            for (Route next : map.get(trace.current)) {
+                if (!trace.routed.contains(next.index)) {
+                    queue.add(new Trace(trace, next));
+                }
+            }
+        }
+
+        System.out.println(-1);
+    }
+
+    public static class Trace implements Comparable<Trace> {
+        private Set<Integer> routed;
+        private int length;
+        private int current;
+
+        public Trace() {
+            this.routed = new HashSet<>();
+            this.length = 0;
+            this.current = 1;
+        }
+
+        public Trace(Trace trace, Route next) {
+            this.routed = new HashSet<>(trace.routed);
+            this.routed.add(next.index);
+
+            this.length = trace.length + next.length;
+            this.current = next.to;
+        }
+
+        public boolean isGoal() {
+            return this.current == 1 && !routed.isEmpty();
+        }
+
+        @Override
+        public int compareTo(Trace o) {
+            return this.length - o.length;
+        }
+    }
+
+    private static class Route {
+        private int index;
+        private int from;
+        private int to;
+        private int length;
+
+        public Route(int index, int from, int to, int length) {
+            this.index = index;
+            this.from = from;
+            this.to = to;
+            this.length = length;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public int getFrom() {
+            return from;
+        }
+
+        public int getTo() {
+            return to;
+        }
+
+        public int getLength() {
+            return length;
+        }
+
+        @Override
+        public String toString() {
+            return "Route{" +
+                    "index=" + index +
+                    ", from=" + from +
+                    ", to=" + to +
+                    ", length=" + length +
+                    '}';
+        }
     }
 
     public static class Scanner {
