bedabf6a10a51b87fd26aa03a2d7debf4f89b830
YujiSoftware <yuji.software+github@gmail.com>
2016-08-16 00:51:15 +0900

    Added ABC008_C (AC, C#)

---
+++ abc008_b/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        string[] S = new string[N];
+        for (int i = 0; i < N; i++)
+        {
+            S[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine(S.GroupBy(s => s).OrderBy(g => g.Count()).Last().Key);
+    }
+}
---
+++ abc008_b/ProgramTest.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace AtCoder
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void 入力例1()
+        {
+            string input =
+@"4
+taro
+jiro
+taro
+saburo";
+            string output =
+@"taro";
+
+            AssertIO(input, output);
+        }
+
+        [TestMethod]
+        public void 入力例2()
+        {
+            string input =
+@"1
+takahashikun";
+            string output =
+@"takahashikun";
+
+            AssertIO(input, output);
+        }
+
+        [TestMethod]
+        public void 入力例3()
+        {
+            string input =
+@"9
+a
+b
+c
+c
+b
+c
+b
+d
+e";
+            string output =
+@"b";
+
+            AssertIO(input, output);
+        }
+
+        private void AssertIO(string input, string output)
+        {
+            StringReader reader = new StringReader(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Program.Main(new string[0]);
+
+            Assert.AreEqual(output + Environment.NewLine, writer.ToString());
+        }
+    }
+}
