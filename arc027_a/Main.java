65b1661be8a7986235ab05cb17594fc9bc22580e
YujiSoftware <yuji.software+github@gmail.com>
2016-08-31 02:12:32 +0900

    Added ARC027_A (AC)

---
+++ arc027_a/Main.java
@@ -0,0 +1,87 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+
+    private static final int MOD = 1_000_000_007;
+    private static final int INF = Integer.MAX_VALUE / 2;
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        int limit = 18 * 60 + 0;
+        int now = h * 60 + m;
+
+        System.out.println(limit - now);
+    }
+
+    public static class Scanner {
+        private BufferedInputStream inputStream;
+
+        public Scanner(InputStream in) {
+            inputStream = new BufferedInputStream(in);
+        }
+
+        public int nextInt() throws IOException {
+            int num = 0;
+
+            int read = skip();
+            do {
+                num = num * 10 + (read - 0x30);
+            } while ((read = inputStream.read()) > 0x20);
+
+            return num;
+        }
+
+        public int[] nextInt(int n) throws IOException {
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextInt();
+            }
+
+            return array;
+        }
+
+        public long nextLong() throws IOException {
+            long num = 0;
+
+            int read = skip();
+            do {
+                num = num * 10 + (read - 0x30);
+            } while ((read = inputStream.read()) > 0x20);
+
+            return num;
+        }
+
+        public long[] nextLong(int n) throws IOException {
+            long[] array = new long[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextLong();
+            }
+
+            return array;
+        }
+
+        public String next() throws IOException {
+            StringBuilder builder = new StringBuilder();
+
+            int read = skip();
+            do {
+                builder.append((char) read);
+            } while ((read = inputStream.read()) > 0x20);
+
+            return builder.toString();
+        }
+
+        private int skip() throws IOException {
+            int read;
+            while ((read = inputStream.read()) <= 0x20)
+                ;
+
+            return read;
+        }
+    }
+}
