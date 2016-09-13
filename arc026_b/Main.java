4750de1e2708efc444b492550caa8e9236f1808f
YujiSoftware <yuji.software+github@gmail.com>
2016-09-14 02:20:02 +0900

    Added ARC026_B (WA)

---
+++ arc026_b/Main.java
@@ -0,0 +1,99 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+    private static final int MOD = 1_000_000_007;
+    private static final int INF = Integer.MAX_VALUE / 2;
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextInt();
+        long sum = 0;
+        for(int i = 1; i <= N / 2; i++){
+            if(N % i == 0){
+                sum += i;
+            }
+        }
+
+        if(sum > N){
+            System.out.println("Abundant");
+        }else if(sum < N){
+            System.out.println("Deficient");
+        }else{
+            System.out.println("Perfect");
+        }
+    }
+
+    public static class Scanner {
+        private BufferedInputStream inputStream;
+
+        public Scanner(InputStream in) {
+            inputStream = new BufferedInputStream(in);
+        }
+
+        public int nextInt() throws IOException {
+            int num = 0;
+
+            int read = skip();
+            do {
+                num = num * 10 + (read - 0x30);
+            } while ((read = inputStream.read()) > 0x20);
+
+            return num;
+        }
+
+        public int[] nextInt(int n) throws IOException {
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextInt();
+            }
+
+            return array;
+        }
+
+        public long nextLong() throws IOException {
+            long num = 0;
+
+            int read = skip();
+            do {
+                num = num * 10 + (read - 0x30);
+            } while ((read = inputStream.read()) > 0x20);
+
+            return num;
+        }
+
+        public long[] nextLong(int n) throws IOException {
+            long[] array = new long[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextLong();
+            }
+
+            return array;
+        }
+
+        public String next() throws IOException {
+            StringBuilder builder = new StringBuilder();
+
+            int read = skip();
+            do {
+                builder.append((char) read);
+            } while ((read = inputStream.read()) > 0x20);
+
+            return builder.toString();
+        }
+
+        private int skip() throws IOException {
+            int read;
+            while ((read = inputStream.read()) <= 0x20)
+                ;
+
+            return read;
+        }
+    }
+}
