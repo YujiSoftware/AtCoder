ecc70f5b579f8a2321512584b73800c124855bff
YujiSoftware <yuji.software+github@gmail.com>
2016-10-03 02:17:04 +0900

    Added AGC005_A (AC)

---
+++ agc005_a/MainTest.java
@@ -0,0 +1,63 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+
+public class MainTest {
+
+    @Test
+    public void 入力例_1() throws Exception {
+        String input =
+                "TSTTSS";
+        String output =
+                "4";
+
+        assertIO(input, output);
+    }
+
+    @Test
+    public void 入力例_2() throws Exception {
+        String input =
+                "SSTTST";
+        String output =
+                "0";
+
+        assertIO(input, output);
+    }
+
+    @Test
+    public void 入力例_3() throws Exception {
+        String input =
+                "TSSTTTSS";
+        String output =
+                "4";
+
+        assertIO(input, output);
+    }
+
+    @Test
+    public void 入力例_4() throws Exception {
+        String input =
+                "SSTSTT";
+        String output =
+                "0";
+
+        assertIO(input, output);
+    }
+
+    private void assertIO(String input, String output) throws Exception {
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        Main.main(new String[0]);
+
+        Assert.assertThat(out.toString(), is(output + System.lineSeparator()));
+    }
+}
