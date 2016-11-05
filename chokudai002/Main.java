31a966570954151b2214238a0e7b363cd0f5caec
YujiSoftware <yuji.software+github@gmail.com>
2016-11-05 12:50:30 +0900

    Updated chokudai002 (AC)

--- chokudai002/Main.java
+++ chokudai002/Main.java
@@ -1,83 +1,47 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.Set;
 import java.util.stream.IntStream;
 
 class Main {
 	public static void main(String[] args) throws IOException {
-		List<Integer> prime = new ArrayList<>();
-		try(Scanner sc = new Scanner(
-				Files.newInputStream(Paths.get("R:\\素数.txt")))){
-			prime.add(sc.nextInt());
+		int step = 50;
+		int[] primes = new int[100000];
+		try (Scanner sc = new Scanner(
+				Files.newInputStream(Paths.get("R:\\素数.txt")))) {
+			for (int i = 0; i < primes.length; i++) {
+				primes[i] = sc.nextInt();
+			}
 		}
-		
-		final int LIMIT = 1000000000;
 
-		List<Pair> list = IntStream.rangeClosed(0, 10000)
-				.parallel()
-				.mapToObj(i -> {
-					int start = LIMIT / 10 - i;
-					int pow = (int) Math.pow(i, 2);
-					int count = 0;
-					for (int j = 2; j < pow; j++) {
-						if (start % j == 0) {
-							count++;
-						}
-					}
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i = 0; set.size() < 100; i += step) {
+			long sum = IntStream.of(primes).skip(i).limit(step).sum();
+			long min = 1;
+			for (int j = 0; j < step; j++) {
+				min *= gcd(sum, primes[i + j]);
+			}
 
-					if (i % 100 == 0) {
-						// System.out.println(start + "," + count);
-						System.out.println(":" + i);
-					}
+			int value = (int) (sum / min);
+			value = (int) (1e9 / value) * value;
 
-					return new Pair(start, count);
-				})
-				.collect(Collectors.toList());
-
-		Collections.sort(list, Comparator.comparing(Pair::getValue).reversed());
-
-		int i = 0;
-		for (Pair p : list) {
-			i++;
-			System.err.println(p.key);
-
-			if (i > 100) {
-				break;
+			if (!set.contains(value)) {
+				System.out.println(value);
+				set.add(value);
 			}
 		}
 	}
 
-	public static class Pair {
-		private int key;
-		private int value;
-
-		public Pair(int key, int value) {
-			super();
-			this.key = key;
-			this.value = value;
+	private static long gcd(long m, long n) {
+		long temp;
+		while (m % n != 0) {
+			temp = n;
+			n = m % n;
+			m = temp;
 		}
-
-		public int getKey() {
-			return key;
-		}
-
-		public int getValue() {
-			return value;
-		}
-
-		public void setValue(int value) {
-			this.value = value;
-		}
-
-		@Override
-		public String toString() {
-			return "{" + key + ", " + value + "}";
-		}
+		return n;
 	}
 }
