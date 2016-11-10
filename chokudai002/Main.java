788a8437c5f27e7bca2aaf529aa90200724da5bc
YujiSoftware <yuji.software+github@gmail.com>
2016-11-11 08:00:21 +0900

    Updated chokuda002 (AC, 3474)

--- chokudai002/Main.java
+++ chokudai002/Main.java
@@ -1,156 +1,51 @@
-import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
-public class Main {
-
-	private static final int MOD = 1000000007;
-
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int W = sc.nextInt();
-		int H = sc.nextInt();
-		int N = sc.nextInt();
-		int[] x = new int[N];
-		int[] y = new int[N];
-		int[] a = new int[N];
-		for (int i = 0; i < N; i++) {
-			x[i] = sc.nextInt();
-			y[i] = sc.nextInt();
-			a[i] = sc.nextInt();
-		}
-
-		int left = 0;
-		int right = W;
-		int bottom = 0;
-		int top = H;
-
-		for (int i = 0; i < N; i++) {
-			switch (a[i]) {
-			case 1:
-				left = Math.max(left, x[i]);
-				break;
-			case 2:
-				right = Math.min(right, x[i]);
-				break;
-			case 3:
-				bottom = Math.max(bottom, y[i]);
-				break;
-			case 4:
-				top = Math.min(top, y[i]);
-				break;
+class Main {
+	public static void main(String[] args) throws IOException {
+		int step = 2;
+		int[] primes = new int[500000];
+		try (Scanner sc = new Scanner(
+				Files.newInputStream(Paths.get("R:\\素数.txt")))) {
+			for (int i = 0; i < primes.length; i++) {
+				primes[i] = sc.nextInt();
 			}
 		}
 
-		System.err.println(left + "," + right + "," + top + "," + bottom);
-		if (right - left < 0 || top - bottom < 0) {
-			System.out.println(0);
-		} else {
-			System.out.println((right - left) * (top - bottom));
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i = 0; set.size() < 100; i += step) {
+			int prime = primes[i + step - 1];
+			OUTER_LOOP: for (int j = prime; j < 1e9; j += prime) {
+				for (int k = 0; k < step - 1; k++) {
+					if (j % primes[i + k] != 0) {
+						continue OUTER_LOOP;
+					}
+				}
+
+				int value = (int) (1e9 / j) * j;
+				value = j;
+
+				if (!set.contains(value)) {
+					System.out.println(value);
+					set.add(value);
+				}
+				break;
+			}
+			// System.err.println(set.size());
 		}
 	}
 
-	public static class Scanner {
-		private BufferedInputStream inputStream;
-
-		public Scanner(InputStream in) {
-			inputStream = new BufferedInputStream(in);
+	private static long gcd(long m, long n) {
+		long temp;
+		while (m % n != 0) {
+			temp = n;
+			n = m % n;
+			m = temp;
 		}
-
-		public int nextInt() throws IOException {
-			int num = 0;
-			int sign = 1;
-
-			int read = skip();
-			if (read == '-') {
-				sign = -1;
-				read = inputStream.read();
-			}
-
-			do {
-				num = num * 10 + sign * (read - 0x30);
-			} while ((read = inputStream.read()) > 0x20);
-
-			return num;
-		}
-
-		public void fill(int[] a) throws IOException {
-			for (int i = 0; i < a.length; i++) {
-				a[i] = nextInt();
-			}
-		}
-
-		public void fill(int[] a, int[] b) throws IOException {
-			if (a.length != b.length) {
-				throw new IllegalArgumentException();
-			}
-
-			for (int i = 0; i < a.length; i++) {
-				a[i] = nextInt();
-				b[i] = nextInt();
-			}
-		}
-
-		public long nextLong() throws IOException {
-			long num = 0;
-			int sign = 1;
-
-			int read = skip();
-			if (read == '-') {
-				sign = -1;
-				read = inputStream.read();
-			}
-
-			do {
-				num = num * 10 + sign * (read - 0x30);
-			} while ((read = inputStream.read()) > 0x20);
-
-			return num;
-		}
-
-		public void fill(long[] a) throws IOException {
-			for (int i = 0; i < a.length; i++) {
-				a[i] = nextLong();
-			}
-		}
-
-		public void fill(long[] a, long[] b) throws IOException {
-			if (a.length != b.length) {
-				throw new IllegalArgumentException();
-			}
-
-			for (int i = 0; i < a.length; i++) {
-				a[i] = nextLong();
-				b[i] = nextLong();
-			}
-		}
-
-		public long[] nextLong(int n) throws IOException {
-			long[] array = new long[n];
-			for (int i = 0; i < n; i++) {
-				array[i] = nextLong();
-			}
-
-			return array;
-		}
-
-		public String next() throws IOException {
-			StringBuilder builder = new StringBuilder();
-
-			int read = skip();
-			do {
-				builder.append((char) read);
-			} while ((read = inputStream.read()) > 0x20);
-
-			return builder.toString();
-		}
-
-		private int skip() throws IOException {
-			int read;
-			while ((read = inputStream.read()) <= 0x20)
-				;
-
-			return read;
-		}
+		return n;
 	}
 }
