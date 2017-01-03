bb454dc50ca6e7e31bfd5797d232176feb40a7b2
YujiSoftware <yuji.software+github@gmail.com>
2017-01-04 08:01:52 +0900

    Added ARC022_A (AC)

---
+++ arc022_a/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String S = sc.nextLine().toUpperCase();
+		char[] array = new char[] { 'I', 'C', 'T' };
+		int index = 0;
+		for (int i = 0; i < S.length(); i++) {
+			char c = S.charAt(i);
+			if (c == array[index]) {
+				if (index == 2) {
+					System.out.println("YES");
+					return;
+				}
+				index++;
+			}
+		}
+		System.out.println("NO");
+	}
+}
