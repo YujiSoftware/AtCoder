52344c433c3b325d36f1588641efc60f3edcb712
YujiSoftware <yuji.software+github@gmail.com>
2017-02-15 08:02:46 +0900

    Added ARC016_A (AC)

---
+++ arc016_a/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+
+		for (int i = 1; i <= N; i++) {
+			if (i != M) {
+				System.out.println(i);
+				break;
+			}
+		}
+	}
+
+	private static boolean isDebug = System.getProperty("sun.desktop") != null;
+
+	private static void debug(Object... o) {
+		if (isDebug) {
+			System.err.println(Arrays.deepToString(o));
+		}
+	}
+}
