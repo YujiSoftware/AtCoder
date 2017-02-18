6f765159fec806a7ca6965e11fccde5f20cbfcec
YujiSoftware <yuji.software+github@gmail.com>
2017-02-19 00:14:11 +0900

    Added ABC055_A (AC)

---
+++ abc055_a/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		System.out.println(N * 800 - (N / 15) * 200);
+	}
+
+	private static boolean isDebug = System.getProperty("sun.desktop") != null;
+
+	private static void debug(Object... o) {
+		if (isDebug) {
+			System.err.println(Arrays.deepToString(o));
+		}
+	}
+}
