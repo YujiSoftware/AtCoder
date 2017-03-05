3d4c326faffe35f7e03f73a1910188aa27e739dc
YujiSoftware <yuji.software+github@gmail.com>
2017-03-06 00:53:23 +0900

    Added yahoo-procon2017-qual_A (AC)

---
+++ yahoo-procon2017-qual_a/Main.java
@@ -0,0 +1,151 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		char[] S = sc.next().toCharArray();
+
+		int[] yahoo = new int[4];
+		for (int i = 0; i < S.length; i++) {
+			switch (S[i]) {
+			case 'y':
+				yahoo[0]++;
+				break;
+			case 'a':
+				yahoo[1]++;
+				break;
+			case 'h':
+				yahoo[2]++;
+				break;
+			case 'o':
+				yahoo[3]++;
+				break;
+			}
+		}
+
+		boolean result = yahoo[0] == 1 && yahoo[1] == 1 && yahoo[2] == 1 && yahoo[3] == 2;
+
+		System.out.println(result ? "YES" : "NO");
+	}
+
+	private static class Buffer {
+		private StringBuilder result = new StringBuilder();
+		private int count = 0;
+
+		public void push(int y, int x) {
+			result.append(y + 1).append(' ').append(x + 1).append(System.lineSeparator());
+		}
+
+		public void end() {
+			count++;
+		}
+
+		public String toString() {
+			return (count) + System.lineSeparator() + result;
+		}
+	}
+
+	private static boolean isDebug = System.getProperty("sun.desktop") != null;
+
+	private static void debug(Object... o) {
+		if (isDebug) {
+			System.err.println(Arrays.deepToString(o));
+		}
+	}
+
+	public static class Scanner {
+		private BufferedInputStream inputStream;
+
+		public Scanner(InputStream in) {
+			inputStream = new BufferedInputStream(in);
+		}
+
+		public int nextInt() throws IOException {
+			int num = 0;
+
+			int read = skip();
+			do {
+				num = num * 10 + (read - 0x30);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return num;
+		}
+
+		public void fill(int[] a) throws IOException {
+			for (int i = 0; i < a.length; i++) {
+				a[i] = nextInt();
+			}
+		}
+
+		public void fill(int[] a, int[] b) throws IOException {
+			if (a.length != b.length) {
+				throw new IllegalArgumentException();
+			}
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = nextInt();
+				b[i] = nextInt();
+			}
+		}
+
+		public long nextLong() throws IOException {
+			long num = 0;
+
+			int read = skip();
+			do {
+				num = num * 10 + (read - 0x30);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return num;
+		}
+
+		public void fill(long[] a) throws IOException {
+			for (int i = 0; i < a.length; i++) {
+				a[i] = nextLong();
+			}
+		}
+
+		public void fill(long[] a, long[] b) throws IOException {
+			if (a.length != b.length) {
+				throw new IllegalArgumentException();
+			}
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = nextLong();
+				b[i] = nextLong();
+			}
+		}
+
+		public long[] nextLong(int n) throws IOException {
+			long[] array = new long[n];
+			for (int i = 0; i < n; i++) {
+				array[i] = nextLong();
+			}
+
+			return array;
+		}
+
+		public String next() throws IOException {
+			StringBuilder builder = new StringBuilder();
+
+			int read = skip();
+			do {
+				builder.append((char) read);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return builder.toString();
+		}
+
+		private int skip() throws IOException {
+			int read;
+			while ((read = inputStream.read()) <= 0x20)
+				;
+
+			return read;
+		}
+	}
+}
