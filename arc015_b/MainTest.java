b6bdf0d7a4118f5e3535a840cef2831501daa33a
YujiSoftware <yuji.software+github@gmail.com>
2017-03-10 08:07:28 +0900

    Added ARC015_B (AC)

---
+++ arc015_b/MainTest.java
@@ -0,0 +1,58 @@
+import static org.hamcrest.CoreMatchers.is;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void 入力例_1() throws Exception {
+		String input = "4" + System.lineSeparator() +
+				"32.2 25.3" + System.lineSeparator() +
+				"36.4 26.4" + System.lineSeparator() +
+				"24.1 18.0" + System.lineSeparator() +
+				"26.0 24.9";
+		String output = "1 1 1 2 0 0";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_2() throws Exception {
+		String input = "3" + System.lineSeparator() +
+				"-2 -5.2" + System.lineSeparator() +
+				"2 -0.1" + System.lineSeparator() +
+				"26.0 -0.1";
+		String output = "0 0 1 0 2 1";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_3() throws Exception {
+		String input = "4" + System.lineSeparator() +
+				"15.0 9.5" + System.lineSeparator() +
+				"12.5 10.5" + System.lineSeparator() +
+				"20.5 19.9" + System.lineSeparator() +
+				"15.5 15.5";
+		String output = "0 0 0 0 0 0";
+
+		assertIO(input, output);
+	}
+
+	private void assertIO(String input, String output) throws Exception {
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		Main.main(new String[0]);
+
+		Assert.assertThat(out.toString(), is(output + System.lineSeparator()));
+	}
+}
