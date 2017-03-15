8c79d237d904e27331c039dec5b98b5efb103f78
YujiSoftware <yuji.software+github@gmail.com>
2017-03-16 01:57:43 +0900

    Added ABC005_C (AC)

---
+++ abc005_c/Main.java
@@ -0,0 +1,139 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int N = sc.nextInt();
+		Deque<Integer> A = new ArrayDeque<>();
+		for (int i = 0; i < N; i++) {
+			A.add(sc.nextInt());
+		}
+		int M = sc.nextInt();
+		Deque<Integer> B = new ArrayDeque<>();
+		for (int i = 0; i < M; i++) {
+			B.add(sc.nextInt());
+		}
+
+		while (!A.isEmpty() && !B.isEmpty()) {
+			int now = A.poll();
+			int time = B.peek();
+			if (now <= time && time <= now + T) {
+				B.poll();
+			}
+		}
+
+		if (B.isEmpty()) {
+			System.out.println("yes");
+		} else {
+			System.out.println("no");
+		}
+	}
+
+	private static boolean isDebug = System.getProperty("sun.desktop") != null;
+
+	private static void debug(Object... o) {
+		if (isDebug) {
+			System.err.println(Arrays.deepToString(o));
+		}
+	}
+
+	public static class Scanner {
+		private BufferedInputStream inputStream;
+		private StringBuilder buffer = new StringBuilder();
+
+		public Scanner(InputStream in) {
+			inputStream = new BufferedInputStream(in);
+		}
+
+		public int nextInt() throws IOException {
+			int num = 0;
+
+			int read = skip();
+			do {
+				num = num * 10 + (read - 0x30);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return num;
+		}
+
+		public void fill(int[] a) throws IOException {
+			for (int i = 0; i < a.length; i++) {
+				a[i] = nextInt();
+			}
+		}
+
+		public void fill(int[] a, int[] b) throws IOException {
+			if (a.length != b.length) {
+				throw new IllegalArgumentException();
+			}
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = nextInt();
+				b[i] = nextInt();
+			}
+		}
+
+		public long nextLong() throws IOException {
+			long num = 0;
+
+			int read = skip();
+			do {
+				num = num * 10 + (read - 0x30);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return num;
+		}
+
+		public void fill(long[] a) throws IOException {
+			for (int i = 0; i < a.length; i++) {
+				a[i] = nextLong();
+			}
+		}
+
+		public void fill(long[] a, long[] b) throws IOException {
+			if (a.length != b.length) {
+				throw new IllegalArgumentException();
+			}
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = nextLong();
+				b[i] = nextLong();
+			}
+		}
+
+		public long[] nextLong(int n) throws IOException {
+			long[] array = new long[n];
+			for (int i = 0; i < n; i++) {
+				array[i] = nextLong();
+			}
+
+			return array;
+		}
+
+		public String next() throws IOException {
+			int read = skip();
+			do {
+				buffer.append((char) read);
+			} while ((read = inputStream.read()) > 0x20);
+
+			String text = buffer.toString();
+			buffer.delete(0, buffer.length());
+
+			return text;
+		}
+
+		private int skip() throws IOException {
+			int read;
+			while ((read = inputStream.read()) <= 0x20)
+				;
+
+			return read;
+		}
+	}
+}
---
+++ abc005_c/MainTest.java
@@ -0,0 +1,83 @@
+import static org.hamcrest.CoreMatchers.is;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void 入力例_1() throws Exception {
+		String input = "1" + System.lineSeparator() +
+				"3" + System.lineSeparator() +
+				"1 2 3" + System.lineSeparator() +
+				"3" + System.lineSeparator() +
+				"2 3 4";
+		String output = "yes";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_2() throws Exception {
+		String input = "1" + System.lineSeparator() +
+				"3" + System.lineSeparator() +
+				"1 2 3" + System.lineSeparator() +
+				"3" + System.lineSeparator() +
+				"2 3 5";
+		String output = "no";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_3() throws Exception {
+		String input = "1" + System.lineSeparator() +
+				"3" + System.lineSeparator() +
+				"1 2 3" + System.lineSeparator() +
+				"10" + System.lineSeparator() +
+				"1 2 3 4 5 6 7 8 9 10";
+		String output = "no";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_4() throws Exception {
+		String input = "1" + System.lineSeparator() +
+				"3" + System.lineSeparator() +
+				"1 2 3" + System.lineSeparator() +
+				"3" + System.lineSeparator() +
+				"1 2 2";
+		String output = "no";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_5() throws Exception {
+		String input = "2" + System.lineSeparator() +
+				"5" + System.lineSeparator() +
+				"1 3 6 10 15" + System.lineSeparator() +
+				"3" + System.lineSeparator() +
+				"4 8 16";
+		String output = "yes";
+
+		assertIO(input, output);
+	}
+
+	private void assertIO(String input, String output) throws Exception {
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		Main.main(new String[0]);
+
+		Assert.assertThat(out.toString(), is(output + System.lineSeparator()));
+	}
+}
