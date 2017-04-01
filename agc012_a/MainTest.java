b6a7a8fb5472dbdf2f5e06a1159fbda6a4d75f18
YujiSoftware <yuji.software+github@gmail.com>
2017-04-02 00:14:53 +0900

    Added AGC012_A (AC)

---
+++ agc012_a/MainTest.java
@@ -0,0 +1,41 @@
+import static org.hamcrest.CoreMatchers.is;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void 入力例_1() throws Exception {
+		String input = "2" + System.lineSeparator() +
+				"5 2 8 5 1 5";
+		String output = "10";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_2() throws Exception {
+		String input = "10" + System.lineSeparator() +
+				"1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000";
+		String output = "10000000000";
+
+		assertIO(input, output);
+	}
+
+	private void assertIO(String input, String output) throws Exception {
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		Main.main(new String[0]);
+
+		Assert.assertThat(out.toString(), is(output + System.lineSeparator()));
+	}
+}
