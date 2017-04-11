2c6aa77d801494cb329d204293ea2f8c0ccd62a7
YujiSoftware <yuji.software+github@gmail.com>
2017-04-12 00:54:48 +0900

    Added ARC006_A (AC)

---
+++ arc006_a/MainTest.java
@@ -0,0 +1,73 @@
+import static org.hamcrest.CoreMatchers.is;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void 入力例_1() throws Exception {
+		String input = "1 2 3 4 5 6" + System.lineSeparator() +
+				"7" + System.lineSeparator() +
+				"1 2 3 4 5 6";
+		String output = "1";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_2() throws Exception {
+		String input = "0 1 3 5 7 9" + System.lineSeparator() +
+				"4" + System.lineSeparator() +
+				"0 2 4 6 8 9";
+		String output = "0";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_3() throws Exception {
+		String input = "0 2 6 7 8 9" + System.lineSeparator() +
+				"4" + System.lineSeparator() +
+				"0 5 6 7 8 9";
+		String output = "3";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_4() throws Exception {
+		String input = "1 3 5 6 7 8" + System.lineSeparator() +
+				"9" + System.lineSeparator() +
+				"3 5 6 7 8 9";
+		String output = "2";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_5() throws Exception {
+		String input = "0 1 3 4 5 7" + System.lineSeparator() +
+				"8" + System.lineSeparator() +
+				"2 3 5 7 8 9";
+		String output = "5";
+
+		assertIO(input, output);
+	}
+
+	private void assertIO(String input, String output) throws Exception {
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		Main.main(new String[0]);
+
+		Assert.assertThat(out.toString(), is(output + System.lineSeparator()));
+	}
+}
