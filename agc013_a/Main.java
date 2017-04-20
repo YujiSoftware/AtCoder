d8d1795bc517f6a457e2f85aebf31ab500eceea5
YujiSoftware <yuji.software+github@gmail.com>
2017-04-20 23:01:18 +0900

    Added AGC013_A (AC)

---
+++ agc013_a/Main.java
@@ -0,0 +1,152 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] A = new int[N];
+		sc.fill(A);
+
+		int count = 0;
+		boolean undefined = true;
+		boolean increase = false;
+		for (int i = 1; i < A.length; i++) {
+			if (A[i - 1] != A[i]) {
+				if (undefined) {
+					increase = (A[i - 1] < A[i]);
+					undefined = false;
+				} else {
+					if ((A[i - 1] < A[i] && !increase)
+							|| A[i - 1] > A[i] && increase) {
+						count++;
+						undefined = true;
+					}
+				}
+			}
+		}
+
+		System.out.println(count + 1);
+	}
+
+	private static boolean isDebug = System.getProperty("sun.desktop") != null;
+
+	private static void debug(Object... o) {
+		if (isDebug) {
+			System.err.println(Arrays.deepToString(o));
+		}
+	}
+
+	public static class Scanner {
+		private BufferedInputStream inputStream;
+
+		public Scanner(InputStream in) {
+			inputStream = new BufferedInputStream(in);
+		}
+
+		public int nextInt() throws IOException {
+			int num = 0;
+			int sign = 1;
+
+			int read = skip();
+			if (read == '-') {
+				sign = -1;
+				read = inputStream.read();
+			}
+
+			do {
+				num = num * 10 + sign * (read - 0x30);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return num;
+		}
+
+		public void fill(int[] a) throws IOException {
+			for (int i = 0; i < a.length; i++) {
+				a[i] = nextInt();
+			}
+		}
+
+		public void fill(int[] a, int[] b) throws IOException {
+			if (a.length != b.length) {
+				throw new IllegalArgumentException();
+			}
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = nextInt();
+				b[i] = nextInt();
+			}
+		}
+
+		public void fill(int[] a, int[] b, int[] c) throws IOException {
+			if (a.length != b.length || b.length != c.length) {
+				throw new IllegalArgumentException();
+			}
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = nextInt();
+				b[i] = nextInt();
+				c[i] = nextInt();
+			}
+		}
+
+		public long nextLong() throws IOException {
+			long num = 0;
+
+			int read = skip();
+			do {
+				num = num * 10 + (read - 0x30);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return num;
+		}
+
+		public void fill(long[] a) throws IOException {
+			for (int i = 0; i < a.length; i++) {
+				a[i] = nextLong();
+			}
+		}
+
+		public void fill(long[] a, long[] b) throws IOException {
+			if (a.length != b.length) {
+				throw new IllegalArgumentException();
+			}
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = nextLong();
+				b[i] = nextLong();
+			}
+		}
+
+		public long[] nextLong(int n) throws IOException {
+			long[] array = new long[n];
+			for (int i = 0; i < n; i++) {
+				array[i] = nextLong();
+			}
+
+			return array;
+		}
+
+		public String next() throws IOException {
+			StringBuilder builder = new StringBuilder();
+
+			int read = skip();
+			do {
+				builder.append((char) read);
+			} while ((read = inputStream.read()) > 0x20);
+
+			return builder.toString();
+		}
+
+		private int skip() throws IOException {
+			int read;
+			while ((read = inputStream.read()) <= 0x20)
+				;
+
+			return read;
+		}
+	}
+}
