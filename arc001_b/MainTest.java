f41d0936ec81f24d38b4b47c11005cd7425102fd
YujiSoftware <yuji.software+github@gmail.com>
2017-06-21 01:40:47 +0900

    Added ARC001_B (AC)

---
+++ arc001_b/MainTest.java
@@ -0,0 +1,79 @@
+import static org.hamcrest.CoreMatchers.is;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void 入力例_1() throws Exception {
+		String input = "7 34";
+		String output = "5";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_2() throws Exception {
+		String input = "19 28";
+		String output = "2";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_3() throws Exception {
+		String input = "19 27";
+		String output = "3";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_4() throws Exception {
+		String input = "19 26";
+		String output = "3";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_5() throws Exception {
+		String input = "19 23";
+		String output = "2";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_6() throws Exception {
+		String input = "19 22";
+		String output = "3";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_7() throws Exception {
+		String input = "19 21";
+		String output = "2";
+
+		assertIO(input, output);
+	}
+
+	private void assertIO(String input, String output) throws Exception {
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		Main.main(new String[0]);
+
+		Assert.assertThat(out.toString(), is(output + System.lineSeparator()));
+	}
+}
