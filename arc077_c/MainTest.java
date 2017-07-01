7c7b936d2962533344ae8f3f1db3464765719edc
YujiSoftware <yuji.software+github@gmail.com>
2017-07-02 03:00:56 +0900

    Added ARC077_C (AC)

---
+++ arc077_c/MainTest.java
@@ -0,0 +1,59 @@
+import static org.hamcrest.CoreMatchers.is;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void 入力例_1() throws Exception {
+		String input = "4" + System.lineSeparator() +
+				"1 2 3 4";
+		String output = "4 2 1 3";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_2() throws Exception {
+		String input = "3" + System.lineSeparator() +
+				"1 2 3";
+		String output = "3 1 2";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_3() throws Exception {
+		String input = "1" + System.lineSeparator() +
+				"1000000000";
+		String output = "1000000000";
+
+		assertIO(input, output);
+	}
+
+	@Test
+	public void 入力例_4() throws Exception {
+		String input = "6" + System.lineSeparator() +
+				"0 6 7 6 7 0";
+		String output = "0 6 6 0 7 7";
+
+		assertIO(input, output);
+	}
+
+	private void assertIO(String input, String output) throws Exception {
+		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+		System.setIn(in);
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		Main.main(new String[0]);
+
+		Assert.assertThat(out.toString(), is(output + System.lineSeparator()));
+	}
+}
